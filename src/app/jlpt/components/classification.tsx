60285b3c49aa74fb7e0e5773459ccf2c7ab0cbf9
김태연 <xodus776@nate.com>
2024-12-12 16:28:12 +0900

    수정

--- src/app/jlpt/components/classification.tsx
+++ src/app/jlpt/components/classification.tsx
@@ -30,11 +30,34 @@ const Classification = (props: ClassificationProps) => {
             <div className="grid grid-cols-4 gap-4 sm:grid-cols-2">
               {classificationInfo.years.map((year: string, yIdx: number) => {
                 return classificationInfo.months.map((month: string, mIdx: number) => {
-                  return (
-                    <button key={`btn-${cIdx}-${yIdx}-${mIdx}`} onClick={handleClick({classification: classificationInfo.classification, year: year, month: month})} className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-                      {['N1','N2','N3'].includes(classData.level) ? `${year}/${month}` : `${year}${month}`}
-                    </button>
-                  );
+                  // N1, N2, N3 일 경우
+                  if(['N1','N2','N3'].includes(classData.level)) {
+                    return (
+                      <button key={`btn-${cIdx}-${yIdx}-${mIdx}`} onClick={handleClick({classification: classificationInfo.classification, year: year, month: month})} className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                        {`${year}/${month}`}
+                      </button>
+                    );
+                  }
+
+                  // N4, N5 일 경우
+                  if(['N4','N5'].includes(classData.level)) {
+                    if(year.toLocaleLowerCase() === 'test') {
+                      return (
+                        <button key={`btn-${cIdx}-${yIdx}-${mIdx}`} onClick={handleClick({classification: classificationInfo.classification, year: year, month: month})} className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                          {`${year}${month}`}
+                        </button>
+                      );  
+                    } else {
+                      //
+                      if(['07', '12'].includes(month)) {
+                        return (
+                          <button key={`btn-${cIdx}-${yIdx}-${mIdx}`} onClick={handleClick({classification: classificationInfo.classification, year: year, month: month})} className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                            {`${year}/${month}`}
+                          </button>
+                        );
+                      }
+                    }
+                  }
                 });
               })}
             </div>
