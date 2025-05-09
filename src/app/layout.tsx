fcb4044aa80379d4ce5d79d8cd5d2d36b218e5f4
kty <xodus776@nate.com>
2025-05-09 12:54:55 +0900

    수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,29 @@ const notoSerifJP = Noto_Serif_JP({
 });
 
 export const metadata: Metadata = {
-  title: "JLPTCODE",
+  title: {
+    template: 'JLPTCODE - %s',
+    default: 'JLPTCODE',
+  },
   authors: [{ name: 'JLPTCODE' }],
   description: "일본어능력시험, 일본어 등급별, 년도별, 과목별 기출문제 풀이와 단어외우기를 학습할수 있고, 해석기능과 채점기능을 제공합니다.",
-  keywords: "일본어능력시험, 일본어, JLPT, JPT, 일본어 기출문제, 기출문제 풀이, 일본어 단어, 단어외우기, 모쿠모쿠 일본어, 일본, 일본여행, 동경, 오사카, 나고야, 일본친구",
+  keywords: [
+    '일본어능력시험', 
+    '일본어', 
+    'JLPT', 
+    'JPT', 
+    '일본어 기출문제', 
+    '기출문제 풀이', 
+    '일본어 단어', 
+    '단어외우기', 
+    '모쿠모쿠 일본어', 
+    '일본', 
+    '일본여행', 
+    '동경', 
+    '오사카', 
+    '나고야', 
+    '일본친구',
+  ]
 };
 
 export default function RootLayout({
