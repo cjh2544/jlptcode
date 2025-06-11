c239c6f88d0f770d0109d0bd26381866e76c3e2f
kty <xodus776@nate.com>
2025-06-11 23:09:57 +0900

    수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
       <section className="pb-20 bg-blueGray-200 -mt-24">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap">
-            <Link scroll={false} href={'/wordToday'} className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+            <Link scroll={false} href={'/wordToday'} className="w-full lg:w-3/12 px-4 text-center">
               <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                 <div className="px-4 py-5 flex-auto">
                   <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
@@ -108,7 +108,7 @@ export default function Home() {
               </div>
             </Link>
 
-            <Link scroll={false} href={'/levelUp'} className="w-full md:w-4/12 px-4 text-center">
+            <Link scroll={false} href={'/levelUp'} className="w-full lg:w-3/12 px-4 text-center">
               <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                 <div className="px-4 py-5 flex-auto">
                   <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
@@ -122,7 +122,7 @@ export default function Home() {
               </div>
             </Link>
 
-            {/* <Link scroll={false} href={'/jlptTest'} className="w-full md:w-4/12 px-4 text-center">
+            <Link scroll={false} href={'/jlptTest'} className="w-full lg:w-3/12 px-4 text-center">
               <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                 <div className="px-4 py-5 flex-auto">
                   <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
@@ -131,9 +131,9 @@ export default function Home() {
                   <h6 className="text-xl font-semibold">JLPT<br/>TEST</h6>
                 </div>
               </div>
-            </Link> */}
+            </Link>
 
-            <Link scroll={false} href={'/word/jlpt'} className="pt-6 w-full md:w-4/12 px-4 text-center">
+            <Link scroll={false} href={'/word/jlpt'} className="w-full lg:w-3/12 px-4 text-center">
               <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                 <div className="px-4 py-5 flex-auto">
                   <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
