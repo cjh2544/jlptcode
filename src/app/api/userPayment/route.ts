f84ecf882f4f7c38de7993ebed2104551ed8e27a
kty <xodus776@nate.com>
2025-06-21 15:42:13 +0900

    수정

--- src/app/api/userPayment/route.ts
+++ src/app/api/userPayment/route.ts
@@ -30,15 +30,15 @@ const UserDeleteFormData = z.object({
   password: z.string().min(6, "비밀번호는 6자이상 입력해 주세요.").max(20, "비밀번호는 최대 20자리까지 입력해 주세요."),
 });
 
-export async function GET(request: NextRequest) {
-  await connectDB();
+// export async function GET(request: NextRequest) {
+//   await connectDB();
 
-  const { searchParams } = new URL(request.url);
+//   const { searchParams } = new URL(request.url);
 
-  const userList = await User.find().select('-password');
+//   const userList = await User.find().select('-password');
 
-  return NextResponse.json(userList)
-}
+//   return NextResponse.json(userList)
+// }
 
 export async function POST(req: NextRequest, res: NextResponse) {
   let resultInfo: {success: boolean, result?: any, message?: string | undefined} = { success: false };
@@ -71,41 +71,38 @@ export async function POST(req: NextRequest, res: NextResponse) {
       });
 
       // 조건: 항목이 input 범위와 겹치면 포함
-      const filtered = userPayInfo.payments.filter((item:any) => {
-        const startDate = new Date(item.startDate);
-        const endDate = new Date(item.endDate);
+      const filtered = userPayInfo?.payments[userPayInfo?.payments.length - 1];
+      let sDate = new Date();
+      let eDate = new Date();
 
-        const yyyyStart = startDate.getFullYear();
-        const mmStart = String(startDate.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작
-        const ddStart = String(startDate.getDate()).padStart(2, '0');
+      if(filtered) {
+        // 유효기간이 남아 있을 경우
+        if(filtered.endDate > new Date()) {
+          sDate = new Date(filtered.startDate.getTime());
+          eDate = new Date(filtered.endDate.getTime());
+        }
+      }
 
-        const formattedStart = `${yyyyStart}-${mmStart}-${ddStart}`;
+      // 결제구분
+      if('M' === userPaymentInfo.paymentType) {
+        // 한달
+        eDate.setMonth(eDate.getMonth() + 1);
+      } else if('Y' === userPaymentInfo.paymentType) {
+        // 1년
+        eDate.setFullYear(eDate.getFullYear() + 1);
+      } else if('P' === userPaymentInfo.paymentType) {
+        // 프리미엄
+        eDate = new Date('9999-12-31');
+      } else if('U' === userPaymentInfo.paymentType) {
+        // 평생회원
+        eDate = new Date('9999-12-31');
+      }
 
-        const yyyyEnd = endDate.getFullYear();
-        const mmEnd = String(endDate.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작
-        const ddEnd = String(endDate.getDate()).padStart(2, '0');
-
-        const formattedEnd = `${yyyyEnd}-${mmEnd}-${ddEnd}`;
-
-        return formattedEnd >= '2025-05-21' && formattedStart <= '2025-04-20'
-      });
-
-      console.log(filtered);
-      
       // 기간 계산
       let paymentInfo: any = {
         paymentType: userPaymentInfo.paymentType,
-        startDate: '',
-        endDate: '',
-      }
-
-      // 한달 결제
-      if('M' === userPaymentInfo.paymentType) {
-        paymentInfo = {
-          ...paymentInfo,
-          startDate: '2025-06-20',
-          endDate: '2025-07-20',
-        }
+        startDate: sDate,
+        endDate: eDate,
       }
 
       // 등록된 정보가 없을 경우
@@ -144,87 +141,87 @@ export async function POST(req: NextRequest, res: NextResponse) {
   return NextResponse.json(resultInfo)
 }  
 
-export async function PATCH(req: NextRequest, res: NextResponse) {
-  let resultInfo: {success: boolean, result?: any, message?: string | undefined} = { success: false };
+// export async function PATCH(req: NextRequest, res: NextResponse) {
+//   let resultInfo: {success: boolean, result?: any, message?: string | undefined} = { success: false };
 
-  const session = await getServerSession();
+//   const session = await getServerSession();
 
-  if(!session?.user?.email) {
-    resultInfo = { success: false, message: '로그인 정보가 없습니다.' };
-  } else {
-    await connectDB();
-    const body = await req.formData();
-    const userInfo = Object.fromEntries(body.entries());
-    const validation = UserUpdateFormData.safeParse(userInfo);
+//   if(!session?.user?.email) {
+//     resultInfo = { success: false, message: '로그인 정보가 없습니다.' };
+//   } else {
+//     await connectDB();
+//     const body = await req.formData();
+//     const userInfo = Object.fromEntries(body.entries());
+//     const validation = UserUpdateFormData.safeParse(userInfo);
 
-    if (validation.success) {
-      const resultUpdate = await User.updateOne({
-        email: session?.user.email
-      }, 
-      {
-        ...userInfo,
-        password: bcrypt.hashSync(userInfo.password as string, Number(BCRYPT_SALT_ROUNDS))
-      });
+//     if (validation.success) {
+//       const resultUpdate = await User.updateOne({
+//         email: session?.user.email
+//       }, 
+//       {
+//         ...userInfo,
+//         password: bcrypt.hashSync(userInfo.password as string, Number(BCRYPT_SALT_ROUNDS))
+//       });
       
-      if(isEmpty(resultUpdate) || isEmpty(resultUpdate.modifiedCount === 0)) {
-        resultInfo = { success: false, message: '처리 되지 않았습니다.' };
-      } else {
-        resultInfo = { success: true, message: '회원정보가 수정 되었습니다.' };
-      }
-    } else {
-      resultInfo = validation;
-    }
-  }
+//       if(isEmpty(resultUpdate) || isEmpty(resultUpdate.modifiedCount === 0)) {
+//         resultInfo = { success: false, message: '처리 되지 않았습니다.' };
+//       } else {
+//         resultInfo = { success: true, message: '회원정보가 수정 되었습니다.' };
+//       }
+//     } else {
+//       resultInfo = validation;
+//     }
+//   }
 
-  return NextResponse.json(resultInfo)
-}
+//   return NextResponse.json(resultInfo)
+// }
 
-export async function DELETE(req: NextRequest, res: NextResponse) {
-  let resultInfo: {success: boolean, result?: any, message?: string | undefined} = { success: false };
+// export async function DELETE(req: NextRequest, res: NextResponse) {
+//   let resultInfo: {success: boolean, result?: any, message?: string | undefined} = { success: false };
 
-  const session = await getServerSession();
+//   const session = await getServerSession();
 
-  if(!session?.user?.email) {
-    resultInfo = { success: false, message: '로그인 정보가 없습니다.' };
-  } else {
-    await connectDB();
-    const body = await req.formData();
-    const userInfo = Object.fromEntries(body.entries());
-    const validation = UserDeleteFormData.safeParse(userInfo);
+//   if(!session?.user?.email) {
+//     resultInfo = { success: false, message: '로그인 정보가 없습니다.' };
+//   } else {
+//     await connectDB();
+//     const body = await req.formData();
+//     const userInfo = Object.fromEntries(body.entries());
+//     const validation = UserDeleteFormData.safeParse(userInfo);
 
-    if (validation.success) {
-      // 회원정보 조회
-      const existUserInfo = await User.findOne({
-        email: session?.user.email,
-        provider: 'credentials'
-      });
+//     if (validation.success) {
+//       // 회원정보 조회
+//       const existUserInfo = await User.findOne({
+//         email: session?.user.email,
+//         provider: 'credentials'
+//       });
 
-      if(isEmpty(existUserInfo)) {
-        resultInfo = { success: false, message: '회원정보가 없습니다.' };
-      } else {
-        const matchPassword = bcrypt.compareSync(userInfo.password as string
-          , existUserInfo.password)
+//       if(isEmpty(existUserInfo)) {
+//         resultInfo = { success: false, message: '회원정보가 없습니다.' };
+//       } else {
+//         const matchPassword = bcrypt.compareSync(userInfo.password as string
+//           , existUserInfo.password)
         
-        if(matchPassword) {
-          // 삭제
-          const deleteInfo = await User.deleteOne({
-            email: session?.user.email,
-            provider: 'credentials'
-          });
+//         if(matchPassword) {
+//           // 삭제
+//           const deleteInfo = await User.deleteOne({
+//             email: session?.user.email,
+//             provider: 'credentials'
+//           });
 
-          if(isEmpty(deleteInfo) || deleteInfo.deletedCount !== 1) {
-            resultInfo = { success: false, message: '처리 되지 않았습니다.' };
-          } else {
-            resultInfo = { success: true, message: '회원정보가 삭제 되었습니다.' };
-          }
-        } else {
-          resultInfo = { success: false, message: '비밀번호가 일치하지 않습니다.' };
-        }
-      }
-    } else {
-      resultInfo = validation;
-    }
-  }
+//           if(isEmpty(deleteInfo) || deleteInfo.deletedCount !== 1) {
+//             resultInfo = { success: false, message: '처리 되지 않았습니다.' };
+//           } else {
+//             resultInfo = { success: true, message: '회원정보가 삭제 되었습니다.' };
+//           }
+//         } else {
+//           resultInfo = { success: false, message: '비밀번호가 일치하지 않습니다.' };
+//         }
+//       }
+//     } else {
+//       resultInfo = validation;
+//     }
+//   }
 
-  return NextResponse.json(resultInfo)
-}
+//   return NextResponse.json(resultInfo)
+// }
--- src/app/member/components/memberRowInfo.tsx
+++ src/app/member/components/memberRowInfo.tsx
@@ -1,7 +1,8 @@
 'use client';
-import React, {memo, MouseEvent, useEffect, useState} from "react";
+import React, {FormEvent, memo, MouseEvent, useEffect, useState} from "react";
 import { format } from "date-fns";
 import { PAYMENT_PERIOD } from "@/app/constants/constants";
+import { z } from "zod";
 
 type MemberRowInfoProps = {
   userInfo: User,
@@ -12,14 +13,53 @@ const MemberRowInfo = (props:MemberRowInfoProps) => {
     userInfo, 
   } = props;
 
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [errors, setErrors] = useState<Array<any> | null>(null)
+  const [isShowConfirm, setShowConfirm] = useState<boolean>(false)
+  const [confirmMsg, setConfirmMsg] = useState<string>('')
+  const [isSuccess, setSuccess] = useState<boolean>(false)
+  // const [userPayInfo, setUserPayInfo] = useState<any>({})
   const [showModal, setShowModal] = useState(false);
 
   const handleClose = () => {
     setShowModal(false);
   }
 
-  const handleSave = (data: any) => {
-    console.log(data);
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true)
+    setErrors(null)
+  
+    try {
+      const formData = new FormData(event.currentTarget);
+      
+      const response = await fetch('/api/userPayment', {
+        method: 'POST',
+        body: formData,
+      })
+
+      const data = await response.json();
+      
+      if(data.success) {
+        // setUserPayInfo(Object.fromEntries(formData));
+        setConfirmMsg(data.message);
+        setShowConfirm(true);
+        setSuccess(true);
+      } else {
+        if(data.error) {
+          setErrors(data.error.issues);
+        } else {
+          setConfirmMsg(data.message);
+          setShowConfirm(true);
+        }
+      }
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        console.log(error.message);
+      }
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
@@ -47,31 +87,48 @@ const MemberRowInfo = (props:MemberRowInfoProps) => {
             <div className={`${showModal ? '' : 'hidden'} backdrop-blur-md drop-shadow-lg fixed inset-0 px-4 flex flex-wrap justify-center items-center w-full h-full z-[1000] before:fixed before:inset-0 before:w-full before:h-full before:bg-[rgba(0,0,0,0.5)] overflow-auto font-[sans-serif]`}>
                 <div className="relative p-4 w-full max-w-2xl max-h-full">
                     <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
-                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
-                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                            {userInfo.name}
-                            </h3>
-                            <button onClick={() => setShowModal(!showModal)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
-                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                </svg>
-                                <span className="sr-only">Close modal</span>
-                            </button>
-                        </div>
-                        <div className="p-4 md:p-5 space-y-4">
-                            {PAYMENT_PERIOD.map((item: any, idx: number) =>{
-                                return (
-                                    <label key={`payment-period-${idx}`}  className="flex items-center mb-4">
-                                        <input type="radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" name={`payment-period`} value={item.value} />
-                                        <span className="ml-2">{item.name}</span>
-                                    </label>
-                                )
-                            })}
-                        </div>
-                        <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b">
-                            <button onClick={() => handleSave('aaaaaaaaaaa')} data-modal-hide="default-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">적용</button>
-                            <button onClick={() => setShowModal(!showModal)} data-modal-hide="default-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">취소</button>
-                        </div>
+                        <form onSubmit={onSubmit}>
+                          <input type="hidden" name="email" value={userInfo?.email ?? ''} />
+                          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
+                              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                              {userInfo.name}
+                              </h3>
+                              <button onClick={() => setShowModal(!showModal)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
+                                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                  </svg>
+                                  <span className="sr-only">Close modal</span>
+                              </button>
+                          </div>
+                          <div className="p-4 md:p-5 space-y-4">
+                              {PAYMENT_PERIOD.map((item: any, idx: number) =>{
+                                  return (
+                                      <label key={`payment-period-${idx}`}  className="flex items-center mb-4">
+                                          <input type="radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" name="paymentType" value={item.value} />
+                                          <span className="ml-2">{item.name}</span>
+                                      </label>
+                                  )
+                              })}
+                          </div>
+                          <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b">
+                            {isLoading ? (
+                              <>
+                                <button disabled type="submit" className="cursor-not-allowed w-full bg-blue-300 text-white font-bold py-2 px-4 rounded focus:outline-none">
+                                  <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                                  </svg>
+                                  처리중...
+                                </button>
+                              </>
+                            ) : (
+                              <>
+                                <button data-modal-hide="default-modal" type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">적용</button>
+                              </>
+                            )}
+                              <button onClick={() => setShowModal(!showModal)} data-modal-hide="default-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">취소</button>
+                          </div>
+                        </form>
                     </div>
                 </div>
             </div>
