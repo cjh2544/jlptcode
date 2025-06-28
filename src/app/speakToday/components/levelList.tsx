b74e0bedf0c36c6d29d59dae3f2cc82f16f1705c
kty <xodus776@nate.com>
2025-06-29 00:12:03 +0900

    수정

--- src/app/speakToday/components/levelList.tsx
+++ src/app/speakToday/components/levelList.tsx
@@ -32,7 +32,7 @@ const studyLevelInfoList = [
 
 const LevelList = (props: LevelListProps) => {
   const {
-    levels = 'N5', level = 'N5', idx = 0
+    levels = 'N5', level, idx = 0
   } = props
   
   const wordTodayInfo =useSpeakTodayStore((state:any) => state.wordTodayInfo);
@@ -93,14 +93,14 @@ const LevelList = (props: LevelListProps) => {
               <span className="h-px flex-1 bg-gray-300"></span>
             </div>
             <div className='grid grid-cols-3 sm:grid-cols-2 items-center justify-center gap-2'>
-              <select id="level" name="level" onChange={handleChange} className="border-0 py-2 placeholder-blueGray-300 text-blueGray-600 bg-white rounded shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
+              <select id="level" name="level" value={level} onChange={handleChange} className="border-0 py-2 placeholder-blueGray-300 text-blueGray-600 bg-white rounded shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
                 {studyLevelInfoList.map((item: any, idx: number) => {
                   return (<option key={item.level} value={item.level}>{item.name}</option>)
                 })}
               </select>
               <select id="study" name="study" onChange={handleChange} className="border-0 py-2 placeholder-blueGray-300 text-blueGray-600 bg-white rounded shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
                 <option value="">선택</option>
-                {(studyList.find((item: any) => item.level === wordTodayInfo.level)?.studies ?? []).map((studyNm: any, idx: number) => {
+                {(studyList.find((item: any) => item.level === level)?.studies ?? []).map((studyNm: any, idx: number) => {
                   return (<option key={idx} value={studyNm}>{studyNm}</option>)
                 })}
               </select>
