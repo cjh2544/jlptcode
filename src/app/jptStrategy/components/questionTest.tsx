49fda46d11f80a1ad23093b55a5d4b27e50d3fbe
kty <xodus776@nate.com>
2025-09-13 11:26:26 +0900

    수정

--- src/app/jptStrategy/components/questionTest.tsx
+++ src/app/jptStrategy/components/questionTest.tsx
@@ -58,11 +58,6 @@ const QuestionTestPage = () => {
                 <input id="show-answer-checkbox" type="checkbox" checked={showAnswer} onChange={() => handleChangeCheck('showAnswer', !showAnswer)} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2" />
                 <label htmlFor="show-answer-checkbox" className="ms-2 text-sm font-medium text-gray-900">정답 바로보기</label>
               </div>
-              <span
-                className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
-              >
-                {`${jptInfo.part}`}
-              </span>
             </div>
             <ModalConfirm type={confirmType} message={confirmMsg} visible={isShowConfirm} onClose={(visible: boolean) => setShowConfirm(visible)} />
           </div>
